7b217664af67592033671049453aeb366278c00f
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-10-23 16:31:48 +0200

    Add frame size calculation

--- lib/membrane_caps_video_raw.ex
+++ lib/membrane_caps_video_raw.ex
@@ -2,6 +2,7 @@ defmodule Membrane.Caps.Video.Raw do
   @moduledoc """
   This module provides caps struct for raw video frames.
   """
+  require Integer
 
   @typedoc """
   Width of single frame in pixels.
@@ -41,4 +42,50 @@ defmodule Membrane.Caps.Video.Raw do
 
   @enforce_keys [:width, :height, :framerate, :format, :aligned]
   defstruct [:width, :height, :framerate, :format, :aligned]
+
+  @doc """
+  Simple wrapper over `frame_size/3`. Returns the size of raw video frame
+  in bytes for the given caps.
+  """
+  @spec frame_size(t()) :: Bunch.Type.try_t(pos_integer)
+  def frame_size(%__MODULE__{format: format, width: width, height: height}) do
+    frame_size(format, width, height)
+  end
+
+  @doc """
+  Returns the size of raw video frame in bytes (without padding).
+
+  It may result in error when dimensions don't fulfill requirements for the given format
+  (e.g. I420 requires both dimensions to be divisible by 2).
+  """
+  @spec frame_size(Raw.format_t(), Raw.width_t(), Raw.height()) :: Bunch.Type.try_t(pos_integer)
+  def frame_size(format, width, height)
+      when format in [:I420, :YV12, :NV12, :NV21] and Integer.is_even(width) and
+             Integer.is_even(height) do
+    # Subsampling by 2 in both dimensions
+    # Y = width * height
+    # V = U = (width / 2) * (height / 2)
+    {:ok, div(width * height * 3, 2)}
+  end
+
+  def frame_size(:I422, width, height) when Integer.is_even(width) do
+    # Subsampling by 2 in horizontal dimension
+    # Y = width * height
+    # V = U = (width / 2) * height
+    {:ok, width * height * 2}
+  end
+
+  def frame_size(format, width, height) when format in [:I444, :RGB] do
+    # No subsampling
+    {:ok, width * height * 3}
+  end
+
+  def frame_size(format, width, height) when format in [:AYUV, :RGBA, :BGRA] do
+    # No subsampling and added alpha channel
+    {:ok, width * height * 4}
+  end
+
+  def frame_size(_, _, _) do
+    {:error, :invalid_dims}
+  end
 end
